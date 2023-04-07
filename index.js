1e5c1302791e23dd1cdad888eaa8b9bafce1335d
joshua.amigogo <joshua.amigogo@gmail.com>
2023-04-07 12:41:47 +0100

    Increase tokens

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/ask", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt,
-      max_tokens: 64,
+      max_tokens: 500,
     });
 
     const completion = response.data.choices[0].text;
