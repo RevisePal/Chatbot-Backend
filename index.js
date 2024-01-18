7ef19fd8d08215eff3a9c6171f3f00bb89ca59b6
joshuasparkes <joshsparkes6@gmail.com>
2024-01-18 10:04:34 +0000

    tweak

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const cors = require("cors");
 const badWords = require("bad-words");
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const app = express();
 app.use(express.json());
